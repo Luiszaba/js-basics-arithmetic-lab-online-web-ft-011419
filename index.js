e83e5a273e45d8bdbfdb74d00ba6c00dcf3486df
Luis Zabala <luis.zabala076@gmail.com>
2019-04-21 16:13:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ You should NOT try to redefine any of the above variables.
 
 // Write your code below this line!
 
-newID = 1000000000 + oldID
+const newID = 1000000000 + oldID
 
-Number.currentAge(2)
+const ageIsValid = Number.ageIsValid(2)
